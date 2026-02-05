7340e9a2876c4eaf38205a309cbb31520e028a63
tanushreedhote99-cmyk <tanushreedhote99@gmail.com>
2026-02-05 23:30:09 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -1,49 +1,46 @@
 import cv2
 import os
 import numpy as np
+import pickle
 
-DATASET_PATH = "students_faces"
-MODEL_PATH = "face_model.yml"
-LABEL_PATH = "labels.npy"
-
-faces_data = []
-labels = []
-label_map = {}
-
-current_label = 0
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-
-for person_name in os.listdir(DATASET_PATH):
-    person_path = os.path.join(DATASET_PATH, person_name)
-    if not os.path.isdir(person_path):
-        continue
-
-    label_map[current_label] = person_name
-
-    for img_name in os.listdir(person_path):
-        img_path = os.path.join(person_path, img_name)
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        if img is None:
-            continue
-
-        faces = face_cascade.detectMultiScale(img, 1.3, 5)
-        for (x, y, w, h) in faces:
-            face = img[y:y+h, x:x+w]
-            face = cv2.resize(face, (200, 200))
-            faces_data.append(face)
-            labels.append(current_label)
-
-    current_label += 1
-
-faces_data = np.array(faces_data)
-labels = np.array(labels)
-
+# Path settings
+path = 'dataset'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.train(faces_data, labels)
+detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-recognizer.save(MODEL_PATH)
-np.save(LABEL_PATH, label_map)
+def getImagesAndLabels(path):
+    faceSamples = []
+    ids = []
+    label_map = {}
+    current_id = 0
 
-print("✅ Training completed successfully")
+    for name in os.listdir(path):
+        person_path = os.path.join(path, name)
+        if os.path.isdir(person_path):
+            if name not in label_map:
+                label_map[current_id] = name
+                current_id += 1
+            
+            for img_name in os.listdir(person_path):
+                img_path = os.path.join(person_path, img_name)
+                img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                if img is None: continue
+                
+                faces = detector.detectMultiScale(img)
+                for (x, y, w, h) in faces:
+                    faceSamples.append(img[y:y+h, x:x+w])
+                    ids.append(current_id - 1)
+                    
+    return faceSamples, ids, label_map
+
+print("🔄 Training faces... Isme thoda time lag sakta hai.")
+faces, ids, labels = getImagesAndLabels(path)
+
+if len(faces) > 0:
+    recognizer.train(faces, np.array(ids))
+    recognizer.write('trainer.yml')
+    with open("labels.pickle", 'wb') as f:
+        pickle.dump(labels, f)
+    print("✅ Training Complete! 'trainer.yml' aur 'labels.pickle' ban gayi hain.")
+else:
+    print("❌ Error: Dataset folder mein images nahi mili!")
